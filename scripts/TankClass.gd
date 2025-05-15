d8447e0f28e204f0cb43277b901eb2e9cb905740
Angel Bitsov <Fxll3n@github.com>
2025-05-15 15:15:00 -0500

    Re-wrote tank class, Will finish later

--- scripts/TankClass.gd
+++ scripts/TankClass.gd
@@ -1,43 +1,14 @@
 class_name Tank
 extends CharacterBody2D
 
-@export var mov_speed: float
-@export var barrel_rot_speed: float
+@export_category("Base Stats")
+@export var move_speed: float
+@export var turret_rotation_speed: float
+@export var max_health: float
+@export var current_health: float
+@export_category("Node Refrences")
+@export var modifiers: Array[Modifier]
 @export var magazine: Array[Bullet]
 
-@export_category("Node Refrences")
-
-@export var turret_scene: PackedScene
-@export var body_scene: PackedScene
-
-var turret: Node2D
-var body: Node2D
-
 func _ready() -> void:
-	turret = turret_scene.instantiate()
-	body = body_scene.instantiate()
-	
-	add_child(turret)
-	add_child(body)
-	
-	for child in body.get_children():
-		if child is Marker2D:
-			turret.position = child.position
-
-func _process(delta: float) -> void:
-	turn_barrel()
-
-func _physics_process(delta: float) -> void:
-	pass
-
-func turn_barrel():
-	var mouse_pos = get_global_mouse_position()
-	turret.look_at(mouse_pos)
-
-func set_up():
-	var groups: Array[StringName] = get_groups()
-	
-	if groups.has("Blue") and not groups.has("Red"):
-		pass
-	elif groups.has("Red") and not groups.has("Blue"):
-		pass
+	name = str(get_multiplayer_authority())
